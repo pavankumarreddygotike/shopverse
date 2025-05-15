5f76ab327d4cccf9d2599e96486188ec1eb97cc7
Pavan Reddy <alwayspawan148@gmail.com>
2025-05-15 08:38:34 +0530

    deleted

--- User_Service/User_Service/src/main/java/com/userservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.userservice.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.userservice.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
